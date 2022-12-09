9910c8840f804d537462a940267d6be52df03165
AburridoUnFinde <120139053+AburridoUnFinde@users.noreply.github.com>
2022-12-09 16:08:45 +0100

    Create setup.py

---
+++ inverse-pyinstaller/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="inverse-pyinstaller",
+    version="1.0.0",
+    description="Convierte .exe en .py",
+    author="AburridoUnFinde",
+    author_email="",
+    packages=["mi_paquete"],
+    install_requires=["numpy"],
+)
